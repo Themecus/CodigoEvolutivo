ae7bc34b3007ecc02e532ad7ab5dc75ad01fa31b
Themecus <127447399+Themecus@users.noreply.github.com>
2024-05-14 20:11:33 -0400

    prueba4

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 System.out.println("Fase1");
         System.out.println("Fase4");
+        System.out.println("Fase42");
      Clase ca= new Clase();
      ca.mensaje();
     }
